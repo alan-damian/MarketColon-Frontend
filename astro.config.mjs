09b771d0731a421252796f8a5e86933562774c80
alan-damian <alan41.dc@gmail.com>
2025-03-21 19:53:11 +0100

    Agregar integración con Supabase para cargar datos de comercios, crear archivo de configuración y actualizar la página de Comercios.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.local' });
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-
   vite: {
     plugins: [tailwindcss()]
   }
